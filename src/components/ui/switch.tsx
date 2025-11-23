481adfbc88ed33694f79c25803b65c5d9118c5ed
pedbad <pb357@cam.ac.uk>
2025-11-23 15:47:02 +0000

    style: improve switch contrast

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent bg-muted transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary",
+      "inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-border/60 bg-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary",
       className,
     )}
     {...props}
